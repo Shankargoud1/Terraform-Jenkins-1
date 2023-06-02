d6d77d0fa80cf144816f086a30a2cbd6427f832c
anusha <shankargajula53@gmail.com>
2023-06-02 15:46:24 +0530

    new

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-07c2a88388bb80eb0" # ap-northeast-1
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "TF1-Instance"
   }
 }
